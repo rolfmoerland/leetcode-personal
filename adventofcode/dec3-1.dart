c2a4e41cb8e72c86f66c7a2b5aaba383661e9470
rolfmoerland <moerland.rolf@gmail.com>
2023-12-10 19:07:29 +0100

    adventofCode dec3-1

--- adventofcode/dec3-1.dart
+++ adventofcode/dec3-1.dart
@@ -1,1 +1,76 @@
+import 'dart:io';
 
+class Hit {
+  int x;
+  int y;
+  int length = 0;
+  int value;
+  Hit(this.x, this.y, this.value) {
+    this.length = value.toString().length;
+  }
+}
+
+List<Hit> getHits(List<String> lines) {
+  List<Hit> hits = [];
+  bool isnumber = false;
+  int startX = 0;
+  int startY = 0;
+
+  for (int y = 0; y < lines.length; y++) {
+    for (int x = 0; x < lines[0].length; x++) {
+      if (lines[y][x].codeUnits[0] >= 48 && lines[y][x].codeUnits[0] < 58) {
+        //This is a number.
+        if (!isnumber) {
+          isnumber = true;
+          startX = x;
+          startY = y;
+        }
+      } else {
+        //This is a dot or something else.
+        if (isnumber) {
+          isnumber = false;
+          hits.add(new Hit(
+              startX, startY, int.parse(lines[y].substring(startX, x))));
+        }
+      }
+    }
+    if (isnumber) {
+      isnumber = false;
+      hits.add(new Hit(startX, startY, int.parse(lines[y].substring(startX))));
+    }
+  }
+  return hits;
+}
+
+bool SurroundedBySpecialCharacter(Hit hit, List<String> lines) {
+  for (int i = hit.y - 1; i <= hit.y + 1; i++) {
+    for (int j = hit.x - 1; j <= hit.x + hit.length; j++) {
+      if (i < 0 || j < 0 || i >= lines.length || j >= lines[0].length) {
+        continue;
+      }
+
+      if (lines[i][j] != '.') {
+        if (lines[i][j].codeUnits[0] >= 48 && lines[i][j].codeUnits[0] < 58) {
+        } else {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
+
+void main() {
+  File file = new File('dec3-1.txt');
+  List<String> lines = file.readAsLinesSync();
+
+  List<Hit> hits = getHits(lines);
+
+  int toReturn = 0;
+  for (Hit hit in hits) {
+    if (SurroundedBySpecialCharacter(hit, lines)) {
+      toReturn += hit.value;
+    }
+  }
+  print(toReturn);
+}
--- solved/1662.dart
+++ solved/1662.dart
@@ -1,15 +1,9 @@
 void main() {
-    Solution().main();
+  Solution().main();
 }
 
 class Solution {
-    void main() {
-        print(
-            arrayStringsAreEqual(["abc"], ["ab", "c"])
-        );
-    }   
-    
-    bool arrayStringsAreEqual(List<String> word1, List<String> word2) {
-        
-    }
+  void main() {
+    //print(arrayStringsAreEqual(["abc"], ["ab", "c"]));
+  }
 }
