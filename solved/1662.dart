ddb9e8ea9ae1218f9068a9d20b24ab50dde7890f
rolfmoerland <moerland.rolf@gmail.com>
2023-12-10 17:47:12 +0100

    1662.dart

--- solved/1662.dart
+++ solved/1662.dart
@@ -1,17 +1,15 @@
-//import 'dart:math';
-
 void main() {
-  Solution().main();
+    Solution().main();
 }
 
 class Solution {
-  void main() {
-    print(""
-        //
+    void main() {
+        print(
+            arrayStringsAreEqual(["abc"], ["ab", "c"])
         );
-  }
-
-  bool arrayStringsAreEqual(List<String> word1, List<String> word2) {
-    return word1.join() == word2.join();
-  }
+    }   
+    
+    bool arrayStringsAreEqual(List<String> word1, List<String> word2) {
+        
+    }
 }
