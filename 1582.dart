13a438809f1e6eb3996ef899db20a5cb35fac574
rolfmoerland <moerland.rolf@gmail.com>
2023-12-13 22:58:23 +0100

    rerun 1582.dart

--- 1582.dart
+++ 1582.dart
@@ -1,3 +1,68 @@
+// //import 'dart:math';
+
+// void main() {
+//   Solution().main();
+// }
+
+// class Solution {
+//   void main() {
+//     print(numSpecial([
+//       [0, 0, 0, 0, 0, 1, 0, 0],
+//       [0, 0, 0, 0, 1, 0, 0, 1],
+//       [0, 0, 0, 0, 1, 0, 0, 0],
+//       [1, 0, 0, 0, 1, 0, 0, 0],
+//       [0, 0, 1, 1, 0, 0, 0, 0]
+//     ]));
+//   }
+
+//   int numSpecial(List<List<int>> mat) {
+//     List<int> row = List.filled(mat[0].length, 0);
+//     List<int> col = List.filled(mat.length, 0);
+
+//     int toReturn = 0;
+
+//     for (int y = 0; y < mat.length; y++) {
+//       for (int x = 0; x < mat[0].length; x++) {
+//         print("handling $y $x, value: ${mat[y][x]}");
+//         String topbar = "--|";
+//         for (int i in row) {
+//           topbar += " $i|";
+//         }
+//         print(topbar);
+//         for (int i in col) {
+//           print("$i |");
+//         }
+//         if (mat[y][x] == 1) {
+//           if (col[y] == 0 && row[x] == 0) {
+//             print("this is still clean!");
+//             col[y] = 1;
+//             row[x] = 1;
+//             toReturn++;
+//           } else {
+//             if (col[y] == 1) {
+//               print("column obscured.");
+//               col[y] = -1;
+//               row[x] = -1;
+//               print("to return was $toReturn, now ${toReturn - 1}");
+//               toReturn--;
+//               print("check: $toReturn");
+//             }
+//             if (row[x] == 1) {
+//               print("row obscured.");
+//               col[y] = -1;
+//               row[x] = -1;
+//               print("to return was $toReturn, now ${toReturn - 1}");
+//               toReturn--;
+//               print("check: $toReturn");
+//             }
+//           }
+//         }
+//       }
+//     }
+//     return toReturn;
+//   }
+// }
+
 //import 'dart:math';
 
 void main() {
@@ -7,40 +72,34 @@ void main() {
 class Solution {
   void main() {
     print(numSpecial([
-      [1, 0, 0],
-      [0, 0, 1],
-      [1, 0, 0]
+      [0, 0, 0, 0, 0, 1, 0, 0],
+      [0, 0, 0, 0, 1, 0, 0, 1],
+      [0, 0, 0, 0, 1, 0, 0, 0],
+      [1, 0, 0, 0, 1, 0, 0, 0],
+      [0, 0, 1, 1, 0, 0, 0, 0]
     ]));
   }
 
   int numSpecial(List<List<int>> mat) {
-    List<int> row = List.filled(mat.length, 0);
-    List<int> col = List.filled(mat[0].length, 0);
+    List<int> row = List.filled(mat[0].length, 0);
+    List<int> col = List.filled(mat.length, 0);
 
-    List<List<int>> lastSpecialRow =
-        List.filled(mat.length, List.filled(mat[0].length, 0));
-    List<List<int>> lastSpecialCol =
-        List.filled(mat.length, List.filled(mat.length, 0));
+    List<List<int>> matches = [];
 
     int toReturn = 0;
 
     for (int y = 0; y < mat.length; y++) {
       for (int x = 0; x < mat[0].length; x++) {
         if (mat[y][x] == 1) {
-          if (lastSpecialCol[y][0] == 0 && lastSpecialRow == 0) {
-            lastSpecialCol[y] = [1, x];
-            lastSpecialRow[x] = [1, y];
-            toReturn++;
-          }
-          if (lastSpecialCol[y][0] == 1) {
-            lastSpecialCol[y][0] = -1;
-            toReturn -= 2;
-          }
-          if (lastSpecialRow[x][0] == -1) {
-            lastSpecialRow[x][0] = -1;
-            toReturn -= 2;
-          }
+          row[x] += 1;
+          col[y] += 1;
+          matches.add([y, x]);
         }
+      }
+    }
+    for (List<int> match in matches) {
+      if ((col[match[0]] == 1) && (row[match[1]] == 1)) {
+        toReturn++;
       }
     }
     return toReturn;
