92d331f745e3f1b4eb00906c4882ab14ce65ba4f
moerland.rolf <moerland.rolf@gmail.com>
2023-12-31 23:35:00 +0100

    adventofcode 2023 day 9-2

--- adventofcode/2023/dec9-2.dart
+++ adventofcode/2023/dec9-2.dart
@@ -4,7 +4,7 @@ List<List<List<String>>> input = [];
 
 void main() {
   File file = new File(
-      'C:\\Users\\Gebruiker\\development\\PortableVSCode\\data\\code\\leetcode\\adventofcode\\2023\\dec9ex.txt');
+      'C:\\Users\\Gebruiker\\development\\PortableVSCode\\data\\code\\leetcode\\adventofcode\\2023\\dec9.txt');
   // ignore: unused_local_variable
   List<String> lines = file.readAsLinesSync();
   List<List<int>> importedlines = [];
@@ -34,11 +34,12 @@ void main() {
     extrapolateLine(inputCase);
   }
 
+  print(input);
   int toReturn = 0;
 
   for (List<List<int>> inputCase in input) {
-    print(inputCase[0].last);
-    toReturn += inputCase[0].last;
+    print(inputCase[0].first);
+    toReturn += inputCase[0].first;
   }
   print(toReturn);
 }
@@ -71,6 +72,6 @@ void listIntExpandToZeroes(List<List<int>> inputCase) {
 void extrapolateLine(List<List<int>> inputCase) {
   inputCase.last.insert(0, 0);
   for (int i = inputCase.length - 2; i >= 0; i--) {
-    inputCase[i].insert(0, inputCase[i + 1].first + inputCase[i].first);
+    inputCase[i].insert(0, inputCase[i].first - inputCase[i + 1].first);
   }
 }
